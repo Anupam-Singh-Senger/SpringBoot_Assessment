ba358e231d2074b82cdf994fc65b936e3215687a
Rahul Ranjan <rahulranjan834057@gmail.com>
2023-11-22 10:45:51 +0530

    update

---
+++ src/main/java/com/cts/eschool/EschoolApplication.java
@@ -0,0 +1,27 @@
+package com.cts.eschool;
+
+import com.cts.eschool.Service.StudentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class EschoolApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext ss = SpringApplication.run(EschoolApplication.class, args);
+
+		StudentService s = ss.getBean(StudentService.class);
+		s.addNewStudentDetail(1, "Ruchi", 76);
+		s.addNewStudentDetail(2, "Anjali", 48);
+		s.addNewStudentDetail(3, "Abhi", 95);
+
+		s.getAllStudentDetails();
+		s.getStudentDetailsById(1);
+		s.updateStudentScore(3,60.8);
+		s.deleteStudentDetails(2);
+		s.getAllStudentDetails();
+	}
+
+}
